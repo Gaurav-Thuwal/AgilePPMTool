fbfb3090dfb79aea5c9d44f80f1d3733e10a566e
gaurav <gauravthuwal9828@gmail.com>
2023-04-04 12:26:03 +0530

    Add existing project files to git

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
 import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
+
+  componentDidMount(){
+    this.props.getProjects();
+  }
+
   render() {
+
+    const projects = this.props.project.projects;
+
     return (
       <div className="projects">
         <div className="container">
@@ -15,7 +26,11 @@ class Dashboard extends Component {
 
               <br />
               <hr />
-              <ProjectItem />
+              {
+                projects.map(project => (
+                  <ProjectItem key={project.id} project={project} />
+                ))
+              }
             </div>
           </div>
         </div>
@@ -24,4 +39,13 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  project: state.project
+})
+
+export default connect(mapStateToProps,{getProjects})(Dashboard);
